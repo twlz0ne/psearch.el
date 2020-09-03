b08e330b14e3ab20a89aa9191b978bbb4295dab9
Gong Qijian <gongqijian@gmail.com>
2020-09-03 11:02:16 +0800

    ci: add .travis.yml & run-test.example.sh

---
+++ run-test.example.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+emacs_bins=(
+    ~/.local/bin/emacs
+    # /Applications/Emacs-27.1.app//Contents/MacOS/Emacs
+    # /Applications/Emacs-26.1.app//Contents/MacOS/Emacs
+    # /Applications/Emacs-25.3.app//Contents/MacOS/Emacs
+    # /Applications/Emacs-25.2.app//Contents/MacOS/Emacs
+    # /Applications/Emacs-25.1.app//Contents/MacOS/Emacs
+)
+
+for emacs_bin in "${emacs_bins[@]}"; do
+    export EMACS=$emacs_bin; make test $@
+done
